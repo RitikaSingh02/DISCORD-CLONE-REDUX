758f0b39ba6df3a9961268a5b479f6ad499fa9cf
RitikaSingh02 <ritika2002singh@gmail.com>
2020-12-05 12:37:16 +0530

    added sidebar Channel component

--- src/SidebarChannel.js
+++ src/SidebarChannel.js
@@ -1,12 +1,13 @@
 import	React from 'react'
 import"./SidebarChannel.css"
+import {setChannelId} from "./features/counter/AppSlice"
 
 function SidebarChannel({id,channel}) 
     {
         return (
-            <div className="sidebarChannel">
+            <div className="sidebarChannel" onClick={setChannelId}>
                 <h4>
-                    <span className="sidebarChannel__hash">#</span>YT
+                    <span className="sidebarChannel__hash">#</span>{channel}
                 </h4>
             {/* {*SidebarChannel*} */}
              </div>)
