158f365d8ff4f9caf0c6e96678f5be5ec89092b9
RitikaSingh02 <ritika2002singh@gmail.com>
2020-12-06 14:51:50 +0530

    added message window components in the chat component

--- src/chat.js
+++ src/chat.js
@@ -1,5 +1,5 @@
 // import { AddCircle, CardGiftcard, EmojiEmotions } from '@material-ui/icons'
-import	React from 'react'
+import	React, { useState ,useEffect } from 'react'
 import "./chat.css"
 import Message from "./Message.js"
 import ChatHeader from "./ChatHeader.js"
@@ -8,23 +8,76 @@ import AddCircleIcon from "@material-ui/icons/AddCircle"
 import GifIcon from "@material-ui/icons/Gif"
 import CardGiftcardIcon from "@material-ui/icons/CardGiftcard"
 import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions"
+import { useSelector } from 'react-redux'
+import { selectChannelId, selectChannelName } from './features/counter/AppSlice'
+import { selectUser } from './features/counter/UserSlice'
+import db from "./firebase"
+import firebase from "firebase"
 
 function Chat() 
     {
+        const user = useSelector(selectUser);
+        const channel_id=useSelector(selectChannelId);
+        const channel_name=useSelector(selectChannelName);
+        const [messageInput,setInput]=useState("");
+        const [messages,setMessages]=useState([]);
+
+        useEffect(() => {
+            if(channel_id)
+            {
+                db.collection('channels').doc(channel_id).collection("message").orderBy(
+                    "timestamp","desc"
+                ).onSnapshot((snapshot)=>(
+                    setMessages(snapshot.docs.map((doc)=>doc.data()))
+                ));
+            }
+          
+        }, [])
+
+
+        const sendMessage=(e)=>{
+            e.preventDefault();
+
+            db.collection("channels").doc(channel_id).collection("message").add({
+                message:messageInput,
+                user:user,
+                timestamp:firebase.firestore.FieldValue.serverTimestamp()
+            });
+            
+            console.log("msg component")
+            setInput("")//to again set the message field to null
+        }
+        console.log(messages)
+
         return (
             <div className="chat_section">
                 {/* <h3>this is the chat sec</h3> */}
             {/* {*Chat*} */}
-            <ChatHeader/>
+            <ChatHeader channelName={channel_name}/>
             <div className="chat_messages">
-                <Message/>
+                {
+                    messages.map((message)=>(
+                        <Message
+                        message={message.message}
+                        user={message.user}
+                        timestamp={message.timestamp}
+                        />
+                    ))
+                }
+                {/* <Message message={message} channelName={channel_name} /> */}
                 </div>
             <div className="chat_input_message">
                 <AddCircleIcon fontSize="large"/>
                 <form>
-                    <input className ="message_input" placeholder="Test channel"/>  
+                    <input value={messageInput} 
+                    disabled={!channel_id}
+                    onChange={(e)=>setInput(e.target.value)} 
+                    className ="message_input" placeholder={`Input text here ${channel_name}`}/>  
                     {/* <SendRoundedIcon/> */}
-                    <button className="chat_input_button" type="submit"><SendRoundedIcon/></button>
+                    <button 
+                    disabled={!channel_id}
+                    onClick={sendMessage}
+                    className="chat_input_button" type="submit"><SendRoundedIcon/></button>
                 </form>
                 <div className="chat_input_icons">
                     <CardGiftcardIcon fontSize="large"/>
