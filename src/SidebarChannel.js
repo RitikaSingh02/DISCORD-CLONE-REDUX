09e9b76cfc4069d9c43eb4e99358aa65e750eb9f
RitikaSingh02 <ritika2002singh@gmail.com>
2020-12-06 14:50:44 +0530

    dispatched channelInfo selector

--- src/SidebarChannel.js
+++ src/SidebarChannel.js
@@ -1,11 +1,21 @@
 import	React from 'react'
 import"./SidebarChannel.css"
-import {setChannelId} from "./features/counter/AppSlice"
+import {setChannelInfo} from "./features/counter/AppSlice"
+import {useDispatch} from "react-redux"
 
 function SidebarChannel({id,channel}) 
     {
+        const dispatch=useDispatch()
+        console.log(id,channel);
+
         return (
-            <div className="sidebarChannel" onClick={setChannelId}>
+            <div className="sidebarChannel" onClick={()=>
+                dispatch(setChannelInfo({
+                  channelId:id,
+                  channelName:channel,  
+                }))
+                // console.log(id)
+            }>
                 <h4>
                     <span className="sidebarChannel__hash">#</span>{channel}
                 </h4>
